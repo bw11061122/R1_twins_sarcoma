b2960e4ca7323f84cce1cd8261a48503b189fc9e
bw11061122 <barbara.walkowiak101@gmail.com>
2024-11-15 13:46:32 +0000

    contamination set andplots update

--- 20241101_contamination.R
+++ 20241101_contamination.R
@@ -44,7 +44,7 @@ twins_dt[, c(twins_PDv38is) := NULL]
 
 # Filter to only include mutations retained for filtering 
 muts = read.table('Data/mutations_include_20241114_568.txt') %>% unlist()
-paste('Number of mutations that passed required filters:', length(muts)) # 658
+paste('Number of mutations that passed required filters:', length(muts)) # 568
 
 # Create dataframe with mutations that passed current filters 
 twins_filtered_dt = twins_dt[mut_ID %in% muts]
@@ -178,21 +178,21 @@ twins_filtered_dt[, sum_normal_PD63383_mtr_vaf := apply(.SD, 1, function(x) min(
 # 2 mutations private to the tumour and absent from normal samples (expected VAF in normal = 0)
 
 # 1 select mutations which are expected to be clonal in the tumour 
-pdf('Results/20241114_p3_hist_658muts_mean_median.pdf', width = 4, height = 4)
-hist(twins_filtered_dt[, vaf_all_tumour], breaks=30, xlab = 'VAF', xlim=c(0,1), main = 'VAF in aggregated tumour samples\n(658 mutations)')
+pdf('Results/20241114_p3_hist_568muts_mean_median.pdf', width = 4, height = 4)
+hist(twins_filtered_dt[, vaf_all_tumour], breaks=30, xlab = 'VAF', xlim=c(0,1), main = 'VAF in aggregated tumour samples\n(568 mutations)')
 abline(v = median(twins_filtered_dt[, vaf_all_tumour]), col = 'blue', lwd = 2)
 abline(v = mean(twins_filtered_dt[, vaf_all_tumour]), col = 'red', lwd = 2)
 dev.off()
 
 # indicate the threshold above which mutations can be considered clonal
-pdf('Results/20241114_p3_hist_658muts.pdf', width = 4, height = 4)
-hist(twins_filtered_dt[, vaf_all_tumour], breaks=30, xlab = 'VAF', xlim=c(0,1), main = 'VAF in aggregated tumour samples\n(658 mutations)')
+pdf('Results/20241114_p3_hist_568muts.pdf', width = 4, height = 4)
+hist(twins_filtered_dt[, vaf_all_tumour], breaks=30, xlab = 'VAF', xlim=c(0,1), main = 'VAF in aggregated tumour samples\n(568 mutations)')
 abline(v = 0.2, col = 'purple', lwd = 3)
 dev.off()
 
 # require VAF in aggregated tumour samples > 0.2 and mutation present in all tumour samples 
 muts_likely_clonal_tumour = twins_filtered_dt[vaf_all_tumour > 0.2 & sum_tumour_vaf==10, mut_ID] %>% unlist()
-paste('Number of likely clonal mutations:', length(muts_likely_clonal_tumour)) # 227 
+paste('Number of likely clonal mutations:', length(muts_likely_clonal_tumour)) # 161 
 
 ######################################################################################################
 # CONTAMINATION
@@ -213,7 +213,7 @@ for (sample in samples_normal){
     labs(x = 'VAF (total tumour)', y = glue('VAF in {sample}'))+
     ggtitle(glue('{sample}'))+
     coord_equal(ratio = 1)
-  ggsave(glue('Results/20241114_p3_vaf_tumour_vs_normal_{sample}_227.pdf'), width=4.5, height=4.5)
+  ggsave(glue('Results/20241114_p3_vaf_tumour_vs_normal_{sample}_161.pdf'), width=4.5, height=4.5)
 }
 
 # Add labels for normal PD63383 samples which are clean (show no contamination) 
@@ -287,7 +287,7 @@ for (sample in samples_tumour){
   dev.off()
 }
 
-# Based on median VAF of 27 tumour-specific mutations, determine fraction of normal and tumour cells in each sample 
+# Based on median VAF of 24 tumour-specific mutations, determine fraction of normal and tumour cells in each sample 
 median_VAFs = sapply(twins_filtered_vaf[mut_ID %in% muts_tumour_specific, 2:23], median) 
 median_VAFs_dt = data.frame(median_VAFs)
 median_VAFs_dt = data.table(median_VAFs_dt %>% rownames_to_column('sample'))
@@ -296,15 +296,12 @@ median_VAFs_dt[, status := factor(fcase(
   sample %in% samples_tumour_vaf, 'tumour'))]
 median_VAFs_dt[, tumour_cell_fraction := 2 * median_VAFs]
 median_VAFs_dt[, normal_cell_fraction := 1 - 2 * median_VAFs]
-median_VAFs_dt[, status := factor(fcase(
-  sample %in% samples_normal_vaf, 'normal',
-  sample %in% samples_tumour_vaf, 'tumour'))]
 median_VAFs_dt[, purity := fcase(
   status == 'normal', normal_cell_fraction,
   status == 'tumour', tumour_cell_fraction)]
 median_VAFs_dt[, purity_est := round(purity, 1)]
 
-# Based on mean VAF of 27 tumour-specific mutations, determine fraction of normal and tumour cells in each sample 
+# Based on mean VAF of 24 tumour-specific mutations, determine fraction of normal and tumour cells in each sample 
 mean_VAFs = sapply(twins_filtered_vaf[mut_ID %in% muts_tumour_specific, 2:23], mean) 
 mean_VAFs_dt = data.frame(mean_VAFs)
 mean_VAFs_dt = data.table(mean_VAFs_dt %>% rownames_to_column('sample'))
@@ -313,9 +310,6 @@ mean_VAFs_dt[, status := factor(fcase(
   sample %in% samples_tumour_vaf, 'tumour'))]
 mean_VAFs_dt[, tumour_cell_fraction := 2 * mean_VAFs]
 mean_VAFs_dt[, normal_cell_fraction := 1 - 2 * mean_VAFs]
-mean_VAFs_dt[, status := factor(fcase(
-  sample %in% samples_normal_vaf, 'normal',
-  sample %in% samples_tumour_vaf, 'tumour'))]
 mean_VAFs_dt[, purity := fcase(
   status == 'normal', normal_cell_fraction,
   status == 'tumour', tumour_cell_fraction)]
@@ -337,26 +331,26 @@ write.table(mean_VAFs_dt, 'Data/20241114_estimates_tumour_cont_24muts_mean.csv',
 twins_filtered_dt[, col_mut_PD62341_2 := fcase(
   vaf_all_normal_PD62341 < 0.05, 'tumour (not in PD62341)',
   vaf_all_normal_PD62341 >= 0.05, 'shared embryonic')]
-table(twins_filtered_dt[mut_ID %in% muts_likely_clonal_tumour, col_mut_PD62341_2]) # 8 tumour
+table(twins_filtered_dt[mut_ID %in% muts_likely_clonal_tumour, col_mut_PD62341_2]) # 7 tumour
 table(twins_filtered_dt[, col_mut_PD62341_2]) # 219 tumour
 
 # select mutations < 0.05 in PD63383
 twins_filtered_dt[, col_mut_PD63383_2 := fcase(
   vaf_all_normal_PD63383 < 0.05, 'tumour (not in PD63383)',
   vaf_all_normal_PD63383 >= 0.05, 'shared embryonic')]
-table(twins_filtered_dt[mut_ID %in% muts_likely_clonal_tumour, col_mut_PD63383_2]) # 35 tumour
+table(twins_filtered_dt[mut_ID %in% muts_likely_clonal_tumour, col_mut_PD63383_2]) # 32 tumour
 table(twins_filtered_dt[, col_mut_PD63383_2]) # 270 tumour
 
 # indicate if mutation fulfills the condition (< 0.05 in PD62341 and < 0.05 in PD63383)
 twins_filtered_dt[, col_mut_both_2 := fcase(
   (col_mut_PD63383_2 == 'tumour (not in PD63383)' & col_mut_PD62341_2 == 'tumour (not in PD62341)'), 'tumour-specific',
   (col_mut_PD63383_2 != 'tumour (not in PD63383)' | col_mut_PD62341_2 != 'tumour (not in PD62341)'), 'shared embryonic')]
-table(twins_filtered_dt[mut_ID %in% muts_likely_clonal_tumour, col_mut_both_2]) # 8 tumour
+table(twins_filtered_dt[mut_ID %in% muts_likely_clonal_tumour, col_mut_both_2]) # 7 tumour
 table(twins_filtered_dt[,col_mut_both_2]) # 197 tumour
 
 # Estimate contamination using the set of tumour-specific mutations
 muts_tumour_specific_2 = twins_filtered_dt[mut_ID %in% muts_likely_clonal_tumour & col_mut_both_2=='tumour-specific', mut_ID] %>% unlist()
-paste('Number of mutations classified as tumour-specific:', length(muts_tumour_specific_2)) # 8
+paste('Number of mutations classified as tumour-specific:', length(muts_tumour_specific_2)) # 7
 
 # Plot the distribution of tumour-specific mutations (VAF in normal sample vs VAF in tumour)
 for (sample in samples_normal){
@@ -375,21 +369,21 @@ for (sample in samples_normal){
     coord_equal(ratio=1)+
     labs(x = 'VAF (total tumour)', y = glue('VAF ({sample})'), col = 'Mutation category')+
     ggtitle(glue('{sample}'))
-  ggsave(glue('Results/20241114_p3_vaf_tumour_vs_normal_{sample}_col_both_8.pdf'), width=6, height=4.5)
+  ggsave(glue('Results/20241114_p3_vaf_tumour_vs_normal_{sample}_col_both_7.pdf'), width=6, height=4.5)
 }
 
 # Plot histogram of VAF for selected mutations in each tumour sample 
 for (sample in samples_tumour){
   s_vaf = paste0(sample, '_VAF')
   sample_vaf_all = twins_filtered_vaf[mut_ID %in% muts_tumour_specific_2, ..s_vaf] %>% unlist()
-  pdf(glue('Results/20241114_p3_hist_tumour_vaf_all_{sample}_8muts.pdf'), width=4.5, height=3.5)
+  pdf(glue('Results/20241114_p3_hist_tumour_vaf_all_{sample}_7muts.pdf'), width=4.5, height=3.5)
   hist(sample_vaf_all, xlab = 'VAF', main = glue('{sample}'), xlim = c(0, 1))
   abline(v=median(sample_vaf_all),col="blue")
   abline(v=mean(sample_vaf_all),col='red')
   dev.off()
 }
 
-# Based on median VAF of 8 tumour-specific mutations, determine fraction of normal and tumour cells in each sample 
+# Based on median VAF of 7 tumour-specific mutations, determine fraction of normal and tumour cells in each sample 
 median_VAFs_2 = sapply(twins_filtered_vaf[mut_ID %in% muts_tumour_specific_2, 2:23], median) 
 median_VAFs_dt_2 = data.frame(median_VAFs_2)
 median_VAFs_dt_2 = data.table(median_VAFs_dt_2 %>% rownames_to_column('sample'))
@@ -398,15 +392,12 @@ median_VAFs_dt_2[, status := factor(fcase(
   sample %in% samples_tumour_vaf, 'tumour'))]
 median_VAFs_dt_2[, tumour_cell_fraction := 2 * median_VAFs_2]
 median_VAFs_dt_2[, normal_cell_fraction := 1 - 2 * median_VAFs_2]
-median_VAFs_dt_2[, status := factor(fcase(
-  sample %in% samples_normal_vaf, 'normal',
-  sample %in% samples_tumour_vaf, 'tumour'))]
 median_VAFs_dt_2[, purity := fcase(
   status == 'normal', normal_cell_fraction,
   status == 'tumour', tumour_cell_fraction)]
 median_VAFs_dt_2[, purity_est := round(purity, 1)]
 
-# Based on mean VAF of 8 tumour-specific mutations, determine fraction of normal and tumour cells in each sample 
+# Based on mean VAF of 7 tumour-specific mutations, determine fraction of normal and tumour cells in each sample 
 mean_VAFs_2 = sapply(twins_filtered_vaf[mut_ID %in% muts_tumour_specific_2, 2:23], mean) 
 mean_VAFs_dt_2 = data.frame(mean_VAFs_2)
 mean_VAFs_dt_2 = data.table(mean_VAFs_dt_2 %>% rownames_to_column('sample'))
@@ -415,9 +406,6 @@ mean_VAFs_dt_2[, status := factor(fcase(
   sample %in% samples_tumour_vaf, 'tumour'))]
 mean_VAFs_dt_2[, tumour_cell_fraction := 2 * mean_VAFs_2]
 mean_VAFs_dt_2[, normal_cell_fraction := 1 - 2 * mean_VAFs_2]
-mean_VAFs_dt_2[, status := factor(fcase(
-  sample %in% samples_normal_vaf, 'normal',
-  sample %in% samples_tumour_vaf, 'tumour'))]
 mean_VAFs_dt_2[, purity := fcase(
   status == 'normal', normal_cell_fraction,
   status == 'tumour', tumour_cell_fraction)]
@@ -425,8 +413,8 @@ mean_VAFs_dt_2[, purity_est := round(purity, 1)]
 
 ######################################################################################################
 # OUTPUT 2: SAVE TABLES WITH ESTIMATES OF NORMAL + TUMOUR CELL FRACTION AND PURITY
-write.table(median_VAFs_dt_2, 'Data/20241114_estimates_tumour_cont_8muts_median.csv', sep = ',', quote=F, row.names=F)
-write.table(mean_VAFs_dt_2, 'Data/20241114_estimates_tumour_cont_8muts_mean.csv', sep = ',', quote=F, row.names=F)
+write.table(median_VAFs_dt_2, 'Data/20241114_estimates_tumour_cont_7muts_median.csv', sep = ',', quote=F, row.names=F)
+write.table(mean_VAFs_dt_2, 'Data/20241114_estimates_tumour_cont_7muts_mean.csv', sep = ',', quote=F, row.names=F)
 
 ######################################################################################################
 # Approach 3: identify deviation from VAF = 1 in tumour samples for reads on retained chr1 / chr18
@@ -440,68 +428,43 @@ PD63383_tumour_chr1_18_apq = twins_filtered_dt[PD63383ap_VAF > 0.7 | PD63383aq_V
 # chr1_69984580_G_A # looks okay
 
 muts_chr1_18_retained = c('chr18_71485446_G_A', 'chr1_60839899_G_A', 'chr1_69984580_G_A')
-PD63383_tumour_chr1_18_apq = PD63383_tumour_chr1_18_apq[mut_ID %in% muts_chr1_18_retained]
 
-# the opposite way is to estimate the proportion of cells which carry a mutation on the lost chr
-PD63383_tumour_chr1_18_apq_lost = twins_filtered_dt[(PD63383ap_VAF < 0.1 | PD63383aq_VAF < 0.1) & vaf_all_normal > 0.3]
-# "chr13_69946674_A_G" # not 100% sure with these as the read mate doesn't map correctly 
-# "chr13_69946688_A_G" # not 100% sure with these as the read mate doesn't map correctly
-# "chr13_69946752_A_G" # not 100% sure with these as the read mate doesn't map correctly
-# "chr18_40915401_C_T" # looks okay
-# "chr18_56506391_G_C" # quite a lot of insertions, wouldn't risk it 
-# "chr18_56725490_T_G" # several different issues 
-# "chr18_60251278_G_A" # looks okay
-# "chr1_111337001_A_G" # not extremely well mapped but I'd take it
-# "chr1_111337295_T_C" # variable mapping (double check?)
-# "chr1_12731185_G_A" # looks okay
-# "chr1_13225701_G_A" # poor mapping   
-# "chr1_13269094_A_G" # poor mapping 
-# "chr1_1646526_A_C" # mapping not perfect but probably fine  
-# "chr1_16884197_C_G" # looks okay
-# "chr1_16892573_C_T" # looks okay
-# "chr1_16893359_G_A" # looks okay 
-# "chr1_16894027_G_A" # very interesting mutation next to this  
-# "chr1_16899420_T_C" # looks okay 
-# "chr1_16902600_C_A" # looks okay  
-# "chr1_16932907_G_A" # looks okay 
-# "chr1_1697008_G_T" # reads with deletion but would still think it's probably fine    
-# "chr1_21423048_C_A" # poor mapping  
-# "chr1_25403253_C_T" # looks okay 
-# "chr1_3074825_G_T" # looks okay 
-# "chr1_41824923_G_T" # looks okay
-# "chr1_46897283_A_T" # mapping isn't great 
-# "chr1_47072883_A_T" # mapping could be better but probably still fine  
-# "chr1_48695863_C_T" # looks okay # there is a mutation on Jbrowse chr1_48695882_T_C that looks good but not in the pileup
-# "chr1_53061735_G_A" # looks okay
+# Based on median VAF of 7 tumour-specific mutations, determine fraction of normal and tumour cells in each sample 
+median_VAFs_3 = sapply(twins_filtered_vaf[mut_ID %in% muts_chr1_18_retained, 2:23], median) 
+median_VAFs_dt_3 = data.frame(median_VAFs_3)
+median_VAFs_dt_3 = data.table(median_VAFs_dt_3 %>% rownames_to_column('sample'))
+median_VAFs_dt_3[, status := factor(fcase(
+  sample %in% samples_normal_vaf, 'normal',
+  sample %in% samples_tumour_vaf, 'tumour'))]
+median_VAFs_dt_3[, tumour_cell_fraction := median_VAFs_3]
+median_VAFs_dt_3[, normal_cell_fraction := 1 - median_VAFs_3]
+median_VAFs_dt_3[, purity := fcase(
+  status == 'normal', normal_cell_fraction,
+  status == 'tumour', tumour_cell_fraction)]
+median_VAFs_dt_3[, purity_est := round(purity, 1)]
 
-muts_chr1_18_lost = c("chr18_40915401_C_T", "chr18_60251278_G_A", "chr1_111337001_A_G", "chr1_12731185_G_A", "chr1_1646526_A_C",
-                      "chr1_16884197_C_G", "chr1_16892573_C_T", "chr1_16893359_G_A", "chr1_16894027_G_A", "chr1_16899420_T_C", 
-                      "chr1_16902600_C_A", "chr1_16932907_G_A", "chr1_25403253_C_T", "chr1_3074825_G_T", "chr1_41824923_G_T",
-                      "chr1_48695863_C_T", "chr1_53061735_G_A")
-PD63383_tumour_chr1_18_apq_lost = PD63383_tumour_chr1_18_apq_lost[mut_ID %in% muts_chr1_18_lost]
-chr18_lost = c("chr18_40915401_C_T", "chr18_60251278_G_A")
-
-# save this to a list / table
-cells_chr1_18_retained = sapply(PD63383_tumour_chr1_18_apq[,2:23], mean) %>% unlist()
-cells_chr18_retained = sapply(PD63383_tumour_chr1_18_apq[mut_ID == 'chr18_71485446_G_A',2:23], mean)
-cells_chr1_retained = sapply(PD63383_tumour_chr1_18_apq[mut_ID != 'chr18_71485446_G_A',2:23], mean)
-cells_chr1_18_lost = sapply(PD63383_tumour_chr1_18_apq_lost[,2:23], mean)
-cells_chr18_lost = sapply(PD63383_tumour_chr1_18_apq_lost[mut_ID %in% chr18_lost,2:23], mean)
-cells_chr1_lost = sapply(PD63383_tumour_chr1_18_apq_lost[!mut_ID %in% chr18_lost,2:23], mean)
-
-cells_est = cbind(cells_chr1_18_retained, cells_chr18_retained, cells_chr1_retained,
-                  cells_chr1_18_lost, cells_chr18_lost, cells_chr1_lost)
-
-cells_est_dt = data.table(cells_est)
-cells_est_dt = data.table(cbind(rownames(cells_est), cells_est_dt))
+# Based on mean VAF of 7 tumour-specific mutations, determine fraction of normal and tumour cells in each sample 
+mean_VAFs_3 = sapply(twins_filtered_vaf[mut_ID %in% muts_chr1_18_retained, 2:23], mean) 
+mean_VAFs_dt_3 = data.frame(mean_VAFs_3)
+mean_VAFs_dt_3 = data.table(mean_VAFs_dt_3 %>% rownames_to_column('sample'))
+mean_VAFs_dt_3[, status := factor(fcase(
+  sample %in% samples_normal_vaf, 'normal',
+  sample %in% samples_tumour_vaf, 'tumour'))]
+mean_VAFs_dt_3[, tumour_cell_fraction := mean_VAFs_3]
+mean_VAFs_dt_3[, normal_cell_fraction := 1 - mean_VAFs_3]
+mean_VAFs_dt_3[, purity := fcase(
+  status == 'normal', normal_cell_fraction,
+  status == 'tumour', tumour_cell_fraction)]
+mean_VAFs_dt_3[, purity_est := round(purity, 1)]
 
 ######################################################################################################
 # OUTPUT 3: SAVE TABLES WITH ESTIMATES OF NORMAL + TUMOUR CELL FRACTION AND PURITY
-write.csv(cells_est_dt, 'Data/20241114_purity_estimates_chr1_18.csv')
+write.table(median_VAFs_dt_3, 'Data/20241114_estimates_tumour_cont_3muts_median_chr1_18.csv', sep = ',', quote=F, row.names=F)
+write.table(mean_VAFs_dt_3, 'Data/20241114_estimates_tumour_cont_3muts_mean_chr1_18.csv', sep = ',', quote=F, row.names=F)
 
 ######################################################################################################
 # Plot fraction of normal and tumour cells in each dataset 
-
+# I will use the estimate from 24 mutations
 
 
 
